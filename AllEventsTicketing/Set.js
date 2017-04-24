b45f14857a6e92891c1213d381228411f04bbe9b
Jeanina Lao <lao13001@byui.edu>
2017-04-24 13:01:17 -0600

    4/24/2017 - Initial Commit

---
+++ AllEventsTicketing/Set.js
@@ -0,0 +1,56 @@
+function Set() {
+	
+	
+	this.intersection = function(listA, listB) {
+    
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+    
+    
+    
+	this.union = function(listA, listB) {
+
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   
+	   /*-------------------------------Insert your code here -------------------------------------*/ 
+	   
+	   return resultList;
+	}
+
+
+
+
+	this.relativeComplement = function(listA, listB) {
+
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   	   
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+
+
+
+	this.symmetricDifference = function(listA, listB) {
+
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}	
+	
+
+}
